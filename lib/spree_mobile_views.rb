51049228e08f80e0ed990d6048985b3376ff9839
Roman Smirnov <roman@railsdog.com>
2011-03-20 23:06:25 +0300

    Added regexp to determine mobile devices from mobile-fu

--- lib/spree_mobile_views.rb
+++ lib/spree_mobile_views.rb
@@ -20,12 +20,17 @@ end
 module SpreeBase
   module InstanceMethods
     protected
-
+    MOBILE_USER_AGENTS = 'palm|blackberry|nokia|phone|midp|mobi|symbian|chtml|ericsson|minimo|' +
+                         'audiovox|motorola|samsung|telit|upg1|windows ce|ucweb|astel|plucker|' +
+                         'x320|x240|j2me|sgh|portable|sprint|docomo|kddi|softbank|android|mmp|' +
+                         'pdxgw|netfront|xiino|vodafone|portalmmm|sagem|mot-|sie-|ipod|up\\.b|' +
+                         'webos|amoi|novarra|cdm|alcatel|pocket|ipad|iphone|mobileexplorer|mobile'
+    
     def mobile_device?
       if cookies[:mobile_param]
         cookies[:mobile_param] == "1"
       else
-        request.user_agent =~ /Mobile|webOS/
+        request.user_agent.to_s.downcase =~ Regexp.new(MOBILE_USER_AGENTS)
       end
     end
 
