2d52bcd630ac14ce1fe593790bacc872c351298b
Roman Smirnov <roman@railsdog.com>
2011-03-20 12:52:07 +0300

    Move Spree::BaseController decorator to SpreeBase module to support Devise.

--- lib/spree_mobile_views.rb
+++ lib/spree_mobile_views.rb
@@ -15,3 +15,43 @@ module SpreeMobileViews
     config.to_prepare &method(:activate).to_proc
   end
 end
+
+
+module SpreeBase
+  module InstanceMethods
+    protected
+
+    def mobile_device?
+      if cookies[:mobile_param]
+        cookies[:mobile_param] == "1"
+      else
+        request.user_agent =~ /Mobile|webOS/
+      end
+    end
+
+    def prepare_for_mobile
+      cookies[:mobile_param] = params[:mobile] if params[:mobile]
+    end
+    
+    def prepend_view_path_if_mobile
+      if mobile_device?
+        prepend_view_path File.join(File.dirname(__FILE__), '..', 'app', 'mobile_views')
+        prepend_view_path File.join(Rails.root, 'app', 'mobile_views')
+      end
+    end
+  end
+  
+  class << self
+
+    def included_with_mobile_views(receiver)
+      included_without_mobile_views(receiver)
+      
+      receiver.send :helper_method, 'mobile_device?'
+      receiver.send :before_filter, 'prepare_for_mobile'
+      receiver.send :before_filter, 'prepend_view_path_if_mobile'
+    end
+    
+    alias_method_chain :included, :mobile_views
+  
+  end
+end
