1100793733c33ca12521e0b8fbc561c9e6c15365
Tomoya Chiba <tomo.asleep@gmail.com>
2012-09-14 10:44:51 +0900

    Remove debug comment

--- app/helpers/spree/base_helper_decorator.rb
+++ app/helpers/spree/base_helper_decorator.rb
@@ -4,7 +4,6 @@ Spree::BaseHelper.module_eval do
     if current_order.nil? or current_order.line_items.empty?
       text = "#{text}: (#{t('empty')})"
     else
-      p "#{current_order.class}"
       text = "#{text}: (#{current_order.item_count}) #{current_order.display_total}"
     end
     link_to text, cart_path, :class => css_class(cart_path)
